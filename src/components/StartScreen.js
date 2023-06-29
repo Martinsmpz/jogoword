3077b2a9cb13d675d793f9e53ad17d0fe7b409cc
Gabriel Martins <kacimaang25@gmail.com>
2023-06-29 03:03:37 -0300

    first commit

---
+++ src/components/StartScreen.js
@@ -0,0 +1,16 @@
+import  './StartScreen.css'
+
+  const StartScreen = ({startGame}) => {
+  return (
+    <div className="start">
+    <h1>StarScreen</h1>
+    <p>Clique no botão abaixo para começar a jogar</p>
+    <button onClick={startGame}>Começar o jogo</button>
+
+   </div>
+  )
+}
+
+export default StartScreen
+
+
